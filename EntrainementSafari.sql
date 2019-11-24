448be14ddd0cace2d168a48707e38a0ce224aa64
CHALET Emerick <emerickchalet@gmail.com>
2019-11-24 23:09:08 +0100

    entrainement epreuve sql premiere partie, bloquer au 6

---
+++ EntrainementSafari.sql
@@ -0,0 +1,46 @@
+story 1
+
+SELECT nom, espece, historique.id ,date
+FROM animaux
+LEFT JOIN historique ON animaux.id = historique.id
+WHERE date < "2003-01-01"
+
+story 2
+
+SELECT animaux.nom,espece,animaux.id,enclos.nom AS "nom enclos"
+FROM animaux
+LEFT JOIN enclos ON animaux.id = enclos.id
+WHERE espece = "girafe"
+
+story 3
+
+SELECT animaux.nom,espece,animaux.id,enclos.niveau
+FROM animaux
+LEFT JOIN enclos ON animaux.id = enclos.id
+GROUP BY id
+HAVING espece IN ("Lion","Orang-Outan")
+
+story 4
+
+SELECT animaux.nom,animaux.espece, animaux.taille, historique.date, historique.id
+FROM animaux
+LEFT JOIN historique ON animaux.id = historique.id
+WHERE espece = "elephant"
+AND taille > "130"
+
+story 5
+
+SELECT enclos.id AS "id enclos", enclos.nom, enclos.niveau,COUNT(id_animal) AS "nombre d'animaux"
+FROM enclos
+LEFT JOIN enclos_animaux
+ON enclos.id = enclos_animaux.id_enclot
+WHERE niveau = "moyen"
+
+story 6 bloquer
+
+SELECT animaux.nom , animaux.espece , animaux.sexe, MAX(enclos_animaux.date_entree) ,MIN(enclos_animaux.date_entree)
+FROM animaux
+LEFT JOIN enclos_animaux
+ON animaux.id = enclos_animaux.id_animal
+WHERE espece = "girafe"
+AND sexe = "M"
